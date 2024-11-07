4f7a9c9a55bd174701f46c6f56220f58aebb449e
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-11-07 18:28:47 +0100

    feat(navigation): add navigation elements to the header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,23 @@
+import Link from "next/link";
 import React from "react";
 
 export default function Header() {
   return (
-    <header className="py-16 bg-customPalette-sageGreen px-4 sm:px-6 lg:px-8 w-full flex flex-col  ">
-      <h1 className="text-4xl font-bold tracking-tight text-customPalette-ivory sm:text-6xl">
-        Restaurant
-      </h1>
-      <p className="mt-4 text-lg text-slate-700">
-        Your place for all your favorite restaurants
-      </p>
+    <header className="py-16  bg-customPalette-sageGreen px-4 sm:px-6 lg:px-8 w-full flex justify-between">
+      <div>
+        <h1 className="text-4xl font-bold tracking-tight text-customPalette-ivory sm:text-6xl">
+          Restaurant
+        </h1>
+        <p className="mt-4 text-lg text-slate-700">
+          Your place for all your favorite restaurants
+        </p>
+      </div>
+      <div>
+        <nav className="flex gap-2 text-customPalette-ivory font-bold">
+          <Link href="/">Home</Link>
+          <Link href="/menu">Menu</Link>
+        </nav>
+      </div>
     </header>
   );
 }

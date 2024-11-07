b74dd3ed8db0a68867240033a1486e70e6b941dc
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-11-07 16:20:09 +0100

    feat(tables): add tables grid & fetch data

---
+++ src/components/TablesMap/TablesMap.tsx
@@ -0,0 +1,18 @@
+import useGetTables from "@/hooks/useGetTables/useGetTables";
+import React from "react";
+import TableItem from "./TableItem/TableItem";
+
+export default function TablesMap() {
+  const { error, loading, tables } = useGetTables();
+  return (
+    <div>
+      {error && <p>Error: {error.message}</p>}
+      {loading && <p>Loading...</p>}
+      <div className="grid md:grid-cols-4 lg:grid-cols-6 grid-cols-3 gap-4 text-gray-950">
+        {tables.map((table) => (
+          <TableItem key={table.id} table={table} />
+        ))}
+      </div>
+    </div>
+  );
+}

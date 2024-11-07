c55a370f1a932bd5e5e44eff7234804ff69ca5a0
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-11-07 18:29:06 +0100

    feat(navigation): add menu

---
+++ src/pages/menu.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Menu() {
+  return (
+    <section className=" min-h-screen grid grid-cols-1 md:grid-cols-2 gap-6 py-12 px-4 sm:px-6 lg:px-8">
+      <div>Menu</div>
+      <div>Form</div>
+    </section>
+  );
+}

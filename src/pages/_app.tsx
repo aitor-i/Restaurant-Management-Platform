46e0cd29510bef0b1873e016b69997f070b75b54
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-11-07 16:20:34 +0100

    feat(tables): add tables grid & footer & header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,22 @@
 import "@/styles/globals.css";
+import { ApolloProvider } from "@apollo/client";
+import client from "@/../apollo-client";
 import type { AppProps } from "next/app";
+import Head from "next/head";
+import Header from "@/components/Header/Header";
+import Footer from "@/components/Footer/Footer";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ApolloProvider client={client}>
+      <Head>
+        <title>Restaurant</title>
+        <meta name="description" content="The best restaurant in the world" />
+        <link rel="icon" href="/icon.png" />
+      </Head>
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </ApolloProvider>
+  );
 }

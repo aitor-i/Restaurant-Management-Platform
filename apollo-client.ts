380a0e454e46855251038759cb59aff7c00df621
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-11-07 17:30:31 +0100

    feat(subscriptions): implement ws subscription to gql

--- apollo-client.ts
+++ apollo-client.ts
@@ -1,10 +1,37 @@
 // lib/apollo-client.ts
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache, split, HttpLink } from "@apollo/client";
+import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
+import { createClient } from "graphql-ws";
+import { getMainDefinition } from "@apollo/client/utilities";
 
-const endpoint = "http://localhost:4000/graphql";
+// HTTP link for queries and mutations
+const httpLink = new HttpLink({
+  uri: "http://localhost:4000/graphql", // Your GraphQL HTTP endpoint
+});
 
+// WebSocket link for subscriptions
+const wsLink = new GraphQLWsLink(
+  createClient({
+    url: "ws://localhost:4000/graphql", // Your GraphQL WS endpoint
+  }),
+);
+
+// Use split to send data to each link depending on operation type
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
+    );
+  },
+  wsLink, // Use wsLink for subscriptions
+  httpLink, // Use httpLink for queries and mutations
+);
+
+// Create the Apollo Client
 const client = new ApolloClient({
-  uri: endpoint,
+  link: splitLink,
   cache: new InMemoryCache(),
 });
 

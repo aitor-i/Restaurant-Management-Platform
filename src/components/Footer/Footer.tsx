f620b8b7e10b47db47ee3fecd1435309822756be
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-11-07 16:17:29 +0100

    feat(footer): add footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="py-16 bg-customPalette-sageGreen px-4 sm:px-6 lg:px-8 w-full flex flex-col  ">
+      <p>All rights reserved</p>
+    </div>
+  );
+}

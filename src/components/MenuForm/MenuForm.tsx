6331012aed2e4c4f72f2c0e66e11eb756ce6c512
Aitor Ibarra <ibarraaitor@protonmail.com>
2024-11-08 10:30:00 +0100

    feat(menu-form): add functionality to category form

--- src/components/MenuForm/MenuForm.tsx
+++ src/components/MenuForm/MenuForm.tsx
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useRef } from "react";
 
 import { Button } from "@/components/Button/Button";
 import { Input } from "@/components/Input/Input";
 import { Select } from "../Select/Select";
+import { useCreateCategory } from "@/hooks/useCreateCategory/useCreateCategory";
 
 export default function MenuForm() {
-  const onSubmitCategory = (event: React.FormEvent<HTMLFormElement>) => {
+  const { error, data, createCategory, loading } = useCreateCategory();
+  const categoryFormRef = useRef<HTMLFormElement>(null);
+  const onSubmitCategory = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const formData = new FormData(event.currentTarget);
     const category = formData.get("category") as string;
-    console.log("Category", category);
+    const response = await createCategory(category);
+
+    if (!response.errors) {
+      formData.delete("category");
+      categoryFormRef.current?.reset();
+    }
   };
 
   return (
@@ -17,7 +25,11 @@ export default function MenuForm() {
       <h2 className="text-3xl font-bold tracking-tight py-12 text-customPalette-sand sm:text-4xl">
         Menu Managment
       </h2>
-      <form className="flex flex-col gap-4" onSubmit={onSubmitCategory}>
+      <form
+        ref={categoryFormRef}
+        className="flex flex-col gap-4"
+        onSubmit={onSubmitCategory}
+      >
         <label htmlFor="category" className="text-2xl">
           Category
         </label>
@@ -27,8 +39,15 @@ export default function MenuForm() {
           type="text"
           placeholder="Category"
         />
-        <Button className="text-customPalette-ivory font-bold" type="submit">
-          Add Category
+        {error && (
+          <p className="text-red-500 text-sm">Error: {error.message}</p>
+        )}
+        <Button
+          disabled={loading}
+          className="text-customPalette-ivory font-bold"
+          type="submit"
+        >
+          {loading ? "Loading..." : "Add Category"}
         </Button>
       </form>
     </div>
